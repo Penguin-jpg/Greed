221a4d53cb513458b5d0f7bc677b154b1ff19644
Penguin.jpg <sky026912@gmail.com>
2023-03-26 13:41:43 +0800

    add flying eye enemy

---
+++ Assets/FlyingEye.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEye : MonoBehaviour
+{
+    public float flightSpeed = 3f;
+    public DetectionZone biteDetectionZone;
+    public BoxCollider2D deathCollider;
+    public List<Transform> waypoints;
+    public float waypointReachedDistance = 0.1f;
+    private Animator animator;
+    private Rigidbody2D rb;
+    private Damageable damageable;
+    private int waypointIndex = 0;
+    private Transform nextWaypoint;
+
+    private bool _hasTarget = false;
+    public bool HasTarget
+    {
+        get { return _hasTarget; }
+        private set
+        {
+            _hasTarget = value;
+            animator.SetBool(AnimationStrings.hasTarget, value);
+        }
+    }
+
+    public bool CanMove
+    {
+        get { return animator.GetBool(AnimationStrings.canMove); }
+    }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        damageable = GetComponent<Damageable>();
+    }
+
+    private void Start()
+    {
+        nextWaypoint = waypoints[waypointIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HasTarget = biteDetectionZone.detectedColliders.Count > 0;
+    }
+
+    private void FixedUpdate()
+    {
+        if (damageable.IsAlive)
+        {
+            if (CanMove)
+            {
+                Fly();
+            }
+            else
+            {
+                rb.velocity = Vector2.zero;
+            }
+        }
+    }
+
+    private void Fly()
+    {
+        Vector2 directionToWaypoint = (nextWaypoint.position - transform.position).normalized;
+        float distance = Vector2.Distance(nextWaypoint.position, transform.position);
+        rb.velocity = directionToWaypoint * flightSpeed;
+        UpdateDirection();
+        if (distance <= waypointReachedDistance)
+        {
+            waypointIndex = (waypointIndex + 1) % waypoints.Count;
+            nextWaypoint = waypoints[waypointIndex];
+        }
+    }
+
+    private void UpdateDirection()
+    {
+        Vector3 localScale = transform.localScale;
+        if (localScale.x > 0)
+        {
+            if (rb.velocity.x < 0)
+            {
+                transform.localScale = new Vector3(-1 * localScale.x, localScale.y, localScale.z);
+
+            }
+        }
+        else if (rb.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(-1 * localScale.x, localScale.y, localScale.z);
+        }
+    }
+
+    public void OnDeath()
+    {
+        rb.gravityScale = 2f;
+        rb.velocity = new Vector2(0, rb.velocity.y);
+        deathCollider.enabled = true;
+    }
+}
