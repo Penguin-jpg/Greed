0872b48458ff2d3d48f0e2818a23aebe165f94d5
Penguin.jpg <sky026912@gmail.com>
2023-03-31 20:06:43 +0800

    add health bar

--- Assets/Scripts/Damageable.cs
+++ Assets/Scripts/Damageable.cs
@@ -7,6 +7,7 @@ public class Damageable : MonoBehaviour
 {
     public UnityEvent<int, Vector2> damageableHit;
     public UnityEvent damageableDeath;
+    public UnityEvent<int, int> healthChanged;
     private Animator animator;
     [SerializeField]
     private int _maxHealth = 100;
@@ -27,6 +28,7 @@ public class Damageable : MonoBehaviour
         set
         {
             _health = value;
+            healthChanged?.Invoke(_health, MaxHealth);
             if(_health <= 0)
             {
                 IsAlive = false;
