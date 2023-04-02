4e0eb9e118e4a9e21f66ba9e5fd46590a0f93d45
Penguin.jpg <sky026912@gmail.com>
2023-04-02 11:26:21 +0800

    將建置平台改到android, 新增攻擊及跳躍按鈕, 新增移動用的joystick

---
+++ Assets/Scripts/Buttons/LeftButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftButton : MonoBehaviour
+{
+    public Player player;
+
+    public void onClick()
+    {
+
+    }
+}
